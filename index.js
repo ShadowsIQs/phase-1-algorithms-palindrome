1dd0f43d6119c2136d5d7843928f4f9678fd5542
Ryan <griffinryan77@gmail.com>
2024-02-17 18:24:14 -0500

    Done

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let flipWord = word.split("").reverse().join("");
+
+  if (flipWord === word){
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
-  Add your pseudocode here
-*/
+  if the word were to be reversed it would still read the same as before.
 
+  if the reversed input is the same as the input
+    return true
+  else
+    return false
+*/
 /*
-  Add written explanation of your solution here
+  First i make a variable and set it to flipWord. afterwards 
+  i take the input and reverse it. than compare flipWord to 
+  the first input given
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +32,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
 }
 
 module.exports = isPalindrome;
